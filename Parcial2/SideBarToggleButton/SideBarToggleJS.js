8c2f75de438d0f78e5f4c4284e791bd457bf12ac
L18100188 <luishernandez0626@outlook.com>
2022-03-30 15:50:01 -0500

    Update

---
+++ Parcial2/SideBarToggleButton/SideBarToggleJS.js
@@ -0,0 +1,8 @@
+document.getElementById("btn").addEventListener("click",alternarmenu)
+
+        function alternarmenu() {
+            
+            document.getElementById("sb").classList.toggle("MenuOculto")
+
+            document.getElementById("ct").classList.toggle("ContenidoMargen")
+        }
