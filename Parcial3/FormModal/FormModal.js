c2f31408cb6210d249c7fae19cd0a79bb3037efe
L18100188 <luishernandez0626@outlook.com>
2022-05-02 15:44:43 -0500

    Update

---
+++ Parcial3/FormModal/FormModal.js
@@ -0,0 +1,24 @@
+$(document).ready(function (){
+    $('#btnAlerta').click(function() {
+        $('#ventanaA').show();
+    });        
+
+
+    $('#btnCerrarA').click(function() {
+        $('#ventanaA').hide();
+    });
+
+    $('#btnJS').click(function() {
+        $('.modal-body').text("Se activo el boton JS");
+        $('#ventanaM').modal();
+    });
+
+    $('#btnSw').click(function() {
+        swal({
+            title: "Good Job",
+            text: "Hiciste clic en el boton",
+            icon: "success",
+
+        });
+    });
+});
