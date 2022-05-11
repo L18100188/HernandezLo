cd62f76b9841c2e63f7e129651e6f39ec241763c
L18100188 <luishernandez0626@outlook.com>
2022-05-11 15:49:12 -0500

    BotonJson

---
+++ Parcial3/BotonJson/getregistro.php
@@ -0,0 +1,16 @@
+<?php
+
+$registro['NumControl'] = '18100188';
+$registro['NombreEs'] = 'Luis Angel';
+$registro['ApellidoPa'] = 'Hernandez';
+$registro['ApellidoMa'] = 'Lopez';
+$registro['FechaNa'] = '18100188';
+$registro['DireccionEs'] = 'Andres Treviño #90';
+$registro['Email'] = 'l18100188@nlaredo.tecnm.mx';
+$registro['Tel'] = '8671269382';
+
+$registroJson = json_encode($registro);
+
+echo $registroJson;
+
+?>
