6ac8907676001d8b8f093b265df4b397a2444b60
HernandezLo <luishernandez0626@outlook.com>
2022-05-23 14:49:30 -0500

    ScriptSQL

---
+++ Parcial3/ScriptSql/ScriptSql.sql
@@ -0,0 +1,19 @@
+create database l18100188;
+
+use l18100188;
+
+create table registro
+(
+  NumControl int not null,
+  NombreEs varchar (30),
+  ApellidoPa varchar (30),
+  ApellidoMa varchar (30),
+  FechaNa varchar (30),
+  DireccionEs varchar(60),
+  Email varchar(60),
+  Tel varchar(30)
+  constraint PE_NumControl Primary key(NumControl)
+ );
+
+ Insert into registro (NumControl, NombreEs, ApellidoPa, ApellidoMa, FechaNa, DireccionEs, Email, Tel) 
+ Values ('18100188', 'Luis Angel', 'Hernandez', 'Lopez', '26-06-2000', 'Andres Treviño #90', 'l18100188@nlaredo.tecnm.mx', '8671269382');
