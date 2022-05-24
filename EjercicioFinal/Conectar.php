6fe894a4e30f33d355b45e11fb546f8fd7da567c
HernandezLo <luishernandez0626@outlook.com>
2022-05-24 07:51:33 -0500

    test

--- EjercicioFinal/Conectar.php
+++ EjercicioFinal/Conectar.php
@@ -6,6 +6,7 @@ $password = '123456';
 
 try {
     $con = new PDO ("mysql:host=$hostname;dbname=$database", $username, $password);
+    echo "Se conecto";
 }catch (PDOException $e)
 {
     echo "Error de conexion a la base de datos +";
