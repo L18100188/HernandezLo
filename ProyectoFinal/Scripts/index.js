e3886842b66aca06f17e4db2fb7a8203a80b6b22
HernandezLo <luishernandez0626@outlook.com>
2022-05-30 12:59:48 -0500

    Update

--- ProyectoFinal/Scripts/index.js
+++ ProyectoFinal/Scripts/index.js
@@ -22,6 +22,48 @@ $(document).ready(function (){
         
       });
 
+
+      $('#btnAg').click(function() {
+        var vnum = $('#NumControl').val();
+        var vnom = $('#NombreEs').val();
+        var vapa = $('#ApellidoPa').val();
+        var vama = $('#ApellidoMa').val();
+        var vfec = $('#FechaNa').val();
+        var vdir = $('#DireccionEs').val();
+        var vema = $('#Email').val();
+        var vtel = $('#Tel').val();
+
+        $.post('php/grabar.php',
+                {nom_doc: vnom_doc, app: vap, nom_cur: vnom_curs, cost: vcost, cate: vcatego, email: vemail, requi: vrequi, desc: vdescrip},
+                function (ret) {
+
+                if (ret['resultado'] != 0) {
+
+                console.log('Error Insercion');
+                alert('ERROR INSERCION');
+                }
+                else {
+
+                $('#idUsuario').val(ret['detalle']);
+                alert('INSERCION REALIZADA');
+                $('#btnNuevo').prop("disabled", false);    
+                $('#btnRegistrar').prop("disabled", true);    
+                $('#btnEliminar').prop("disabled", false); 
+            }
+        },'json');
+
+      });
+
+      $('#btnMod').click(function() {
+        let parid=prompt("Teclee el Numero de Control a modificar");
+
+        $.post('./php/Consulta.php',{par1:parid},function(data){
+          refrescar(data);
+          },'json');
+
+      });
+
+
     
 
     function refrescar(objeto) {
---
+++ ProyectoFinal/php/Modificar.php
@@ -0,0 +1,38 @@
+<?php
+
+$parid = $_POST['par1'];
+$hostname = 'localhost'; //serverName\instanceName
+$database = 'l18100188';
+$username = 'root';
+$password = '123456';
+
+try {
+    $con = new PDO ("mysql:host=$hostname;dbname=$database", $username, $password);
+}catch (PDOException $e)
+{
+    echo "Error de conexion a la base de datos +";
+    echo $e->getMessage();
+    exit();
+}
+
+//$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+try {
+    $consultaSql = "UPDATE registro SET nombreES=.$parid NumControl=".$parid;   
+    $consulta = $con -> prepare($consultaSql);
+    $consulta -> execute();
+    $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+    $res_utf8 = array_map('utf8_encode', $resultado);
+    $consulta->closeCursor();
+    
+
+}catch(PDOException $e)
+{
+    echo "ERROR";
+    echo $e-> getMessage();
+}
+
+
+echo json_encode($res_utf8); 
+  
+?>
